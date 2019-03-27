32130687aaf28d4fbca7514f88318a63f2d8d7b2
eggbee <ty39563956@gmail.com>
2019-03-27 15:15:17 +0900

    [UPDATE] fragment_movie scrollview modified

--- app/src/main/java/com/example/app_movie/Main/MainActivity.kt
+++ app/src/main/java/com/example/app_movie/Main/MainActivity.kt
@@ -8,34 +8,29 @@ import com.example.app_movie.Main.Movie.MovieFragment
 import com.example.app_movie.R
 
 class MainActivity : AppCompatActivity() {
-    internal var movieFragment = MovieFragment()
-    internal var searchFragment = SearchFragment()
-    internal var recommendFragment = RecommendFragment()
-    internal var myPageFragment = MyPageFragment()
     override fun onCreate(savedInstanceState: Bundle?) {
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_main)
-        // 첫 화면 지정
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.frag_main, movieFragment).commitAllowingStateLoss()
 
-        // bottomNavigationView의 아이템이 선택될 때 호출될 리스너 등록
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frag_main, MovieFragment()).commitAllowingStateLoss()
+
         bottomNavigationView.setOnNavigationItemSelectedListener { item ->
             val transaction = supportFragmentManager.beginTransaction()
             when (item.itemId) {
                 R.id.action_one -> {
-                    transaction.replace(R.id.frag_main, movieFragment).commitAllowingStateLoss()
+                    transaction.replace(R.id.frag_main, MovieFragment()).commitAllowingStateLoss()
                 }
                 R.id.action_two -> {
-                    transaction.replace(R.id.frag_main, searchFragment).commitAllowingStateLoss()
+                    transaction.replace(R.id.frag_main, SearchFragment()).commitAllowingStateLoss()
                 }
                 R.id.action_three -> {
-                    transaction.replace(R.id.frag_main, recommendFragment).commitAllowingStateLoss()
+                    transaction.replace(R.id.frag_main, RecommendFragment()).commitAllowingStateLoss()
                 }
                 R.id.action_four -> {
-                    transaction.replace(R.id.frag_main, myPageFragment).commitAllowingStateLoss()
+                    transaction.replace(R.id.frag_main, MyPageFragment()).commitAllowingStateLoss()
                 }
             }
             true
