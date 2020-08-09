7ee98ad470cbec123510d1f2be36ff09c9ea4949
Taeyoung <ty39563956@gmail.com>
2020-08-10 01:54:36 +0900

    Fix deprecated code

--- app/src/main/java/com/example/app_movie/Video/VideoActivity.kt
+++ app/src/main/java/com/example/app_movie/Video/VideoActivity.kt
@@ -2,67 +2,70 @@ package com.example.app_movie.Video
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.android.exoplayer2.ExoPlayerFactory
 import com.google.android.exoplayer2.SimpleExoPlayer
-import com.google.android.exoplayer2.ext.ima.ImaAdsLoader
-import com.google.android.exoplayer2.source.ExtractorMediaSource
-import com.google.android.exoplayer2.source.ads.AdsMediaSource
-import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
-import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
-import com.google.android.exoplayer2.util.Util
 import kotlinx.android.synthetic.main.activity_video.*
 import android.content.pm.ActivityInfo
 import com.example.app_movie.R
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory
 
 
 class VideoActivity : AppCompatActivity() {
 
     private var player: SimpleExoPlayer? = null
-    private lateinit var adsLoader: ImaAdsLoader
+    private var playbackPosition = 0L
+    private var currentWindow = 0
+    private var playWhenReady = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_video)
-        adsLoader = ImaAdsLoader(this, VideoModel.AD_TAG_URI)
     }
 
-    override fun onStart() {
-        super.onStart()
-        player = ExoPlayerFactory.newSimpleInstance(this, DefaultTrackSelector())
-        playerView.player = player
+    private fun initializePlayer() {
+        if (player == null) {
+            player = SimpleExoPlayer.Builder(this).build()
+            playerView.player = player
+            playerView.controllerAutoShow= true
+            val defaultHttpDataSourceFactory =
+                DefaultHttpDataSourceFactory(getString(R.string.app_name))
+            val mediaSource = ProgressiveMediaSource.Factory(defaultHttpDataSourceFactory)
+                .createMediaSource(VideoModel.MP4_URI)
+            player!!.prepare(mediaSource)
+            player!!.seekTo(currentWindow, playbackPosition)
+            player!!.playWhenReady = playWhenReady
+        }
+    }
 
-        val dataSourceFactory = DefaultDataSourceFactory(
-            this,
-            Util.getUserAgent(this, getString(R.string.app_name))
-        )
+    private fun releasePlayer() {
+        player?.let {
+            playbackPosition = it.currentPosition
+            currentWindow = it.currentWindowIndex
+            playWhenReady = it.playWhenReady
+            it.release()
+            player = null
+        }
+    }
 
-        val mediaSource = ExtractorMediaSource.Factory(dataSourceFactory)
-            .createMediaSource(VideoModel.MP4_URI)
 
-        val adsMediaSource = AdsMediaSource(
-            mediaSource, dataSourceFactory, adsLoader,
-            playerView.overlayFrameLayout
-        )
-
-        player?.prepare(adsMediaSource)
-        player?.playWhenReady = true
-
+    override fun onResume() {
+        super.onResume()
+        initializePlayer()
     }
 
     override fun onRestart() {
         super.onRestart()
-        player?.release()
+        initializePlayer()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        releasePlayer()
     }
 
     override fun onStop() {
         super.onStop()
-        player?.release()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-
-        adsLoader.release()
+        releasePlayer()
     }
 }
